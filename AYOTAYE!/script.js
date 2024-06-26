ff223ddbfecf91afb3f2d0cde60650e71936c129
NARodman <ninaalanier8@gmail.com>
2024-06-25 21:02:09 -0400

    Circle Animation Update

--- AYOTAYE!/script.js
+++ AYOTAYE!/script.js
@@ -40,4 +40,23 @@ setInterval(changeText, 3000);
 
 // Circle Skill /////
 
-const circle = document.querySelectorAll('.circle');
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+
+    for(let i = 0 ; i < dots ; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent ; i++){
+        pointsMarked[i].classList.add('marked');
+    }
+})

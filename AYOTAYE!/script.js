2a4e5dac6575285225ca45ce0d78ab0a2697d607
NARodman <ninaalanier8@gmail.com>
2024-07-11 15:10:21 -0400

    Update script.js
    toggle icon navbar

--- AYOTAYE!/script.js
+++ AYOTAYE!/script.js
@@ -136,3 +136,6 @@ const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky", window.scrollY > 50)
 })
+
+//toggle icon navbar////
+
